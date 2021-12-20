4afb04ed492eade335fb9449e0f4defe513d8de9
Thiago Lacerda Donolato <tlacerda0503@gmail.com>
2021-12-19 23:14:32 -0300

    'chapterII'

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,17 +1,52 @@
 import { parse } from "csv-parse";
 import fs from "fs";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
 
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    const stream = fs.createReadStream(file.path);
+  // eslint-disable-next-line no-useless-constructor
+  constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    const parseFile = parse();
+  // eslint-disable-next-line class-methods-use-this
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
 
-    stream.pipe(parseFile);
+      const parseFile = parse();
+      stream.pipe(parseFile);
 
-    parseFile.on("data", async (line) => {
-      console.log(line);
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+          categories.push({ name, description });
+        })
+        .on("end", () => {
+          fs.promises.unlink(file.path); // Remover da app o arquivo da pasta tmp
+          resolve(categories);
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    categories.map(async (category) => {
+      const { name, description } = category;
+      const existCategory = this.categoriesRepository.findByName(name);
+
+      if (!existCategory) {
+        this.categoriesRepository.create({ name, description });
+      }
     });
   }
 }
+// eslint-disable-next-line import/prefer-default-export
 export { ImportCategoryUseCase };
